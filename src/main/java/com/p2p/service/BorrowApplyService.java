bb01976f06988c8cb478e753a79401813fcf0eab
zc7025 <1531952273@qq.com>
2017-12-25 09:43:18 +0800

    借款基本信息，借款详情，借款审核，投资，还款计划和收款计划、资金流向记录，用户资金表的增删改

--- src/main/java/com/p2p/service/BorrowApplyService.java
+++ src/main/java/com/p2p/service/BorrowApplyService.java
@@ -1,5 +1,8 @@
 package com.p2p.service;
 
+import java.util.Date;
+
 public interface BorrowApplyService extends BaseService {
 
+    int updateChecked(Integer baid, Integer status, Date cktime);
 }
