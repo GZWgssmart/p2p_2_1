e06ad7bf5a999afe1fbc60c7106849afc85beaea
ChenChunFa <2514662592@qq.com>
2017-12-27 10:55:34 +0800

    公告

--- src/main/java/com/p2p/dao/NoticeMapper.java
+++ src/main/java/com/p2p/dao/NoticeMapper.java
@@ -1,7 +1,19 @@
 package com.p2p.dao;
 
 import com.p2p.bean.Notice;
+import com.p2p.common.Pager;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface NoticeMapper extends BaseDAO {
+    @Override
+    List<Object> listPagerCriteria(@Param("pager") Pager pager, @Param("notice") Object obj);
+
+    @Override
+    Long countCriteria(@Param("notice") Object obj);
+
 
 }
