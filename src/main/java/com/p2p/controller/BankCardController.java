6e3adf6d12d22f2ff5c75eaa0413b57f585ec4c2
Rayarke <643703243@qq.com>
2017-12-27 20:24:56 +0800

    银行卡的添加完成

--- src/main/java/com/p2p/controller/BankCardController.java
+++ src/main/java/com/p2p/controller/BankCardController.java
@@ -5,10 +5,7 @@ import com.p2p.common.ServerResponse;
 import com.p2p.service.BankCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
 import java.util.List;
@@ -18,40 +15,37 @@ import java.util.List;
  *
  * @author ChuYiming
  */
-@Controller
+@RestController
 @RequestMapping("/data/bankCard")
 public class BankCardController {
 
     @Autowired
     private BankCardService bankCardService;
 
-    @PostMapping("save")
-    public ServerResponse save(BankCard bankCard) {
-        bankCard.setBktime(Calendar.getInstance().getTime());
-        return bankCardService.save(bankCard);
-    }
-
     @PostMapping("remove/{bcid}")
-    @ResponseBody
     public ServerResponse removeById(@PathVariable("bcid") Integer bcid) {
         return bankCardService.removeById(bcid);
     }
 
     @PostMapping("active/{bcid}/{status}")
-    @ResponseBody
     public ServerResponse activeById(@PathVariable("bcid") Integer bcid, @PathVariable("status") Integer status) {
         return bankCardService.active(bcid, status);
     }
 
     @RequestMapping("allCards")
-    @ResponseBody
     public List<BankCard> allCards(Integer uid) {
         System.out.println(uid);
         return bankCardService.bankCardsByUid(uid);
     }
-    @RequestMapping()
-    @ResponseBody
+    @RequestMapping("add")
     public ServerResponse addCard(BankCard bankCard){
-        return bankCardService.save(bankCard);
+        ServerResponse serverResponse = null;
+        if(bankCard!=null){
+            bankCardService.save(bankCard);
+            serverResponse = ServerResponse.createBySuccess();
+        } else {
+            serverResponse = ServerResponse.createByError();
+        }
+          return serverResponse;
     }
 }
