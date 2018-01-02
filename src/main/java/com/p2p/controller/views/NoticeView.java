d0516eb329a2d8e38ef3256c0c1884bd110f97ea
ChenChunFa <2514662592@qq.com>
2018-01-02 16:38:30 +0800

    修改

--- src/main/java/com/p2p/controller/views/NoticeView.java
+++ src/main/java/com/p2p/controller/views/NoticeView.java
@@ -11,11 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class NoticeView {
 
     @RequestMapping("allNotice")
-    public String allNotie(){
+    public String allNotice(){
         return "message/allNotice";
     }
     @RequestMapping("addNotice")
     public String addNotice(){
         return "message/addNotice";
+    }
+
+    @RequestMapping("editNotice")
+    public String editNotice(){
+        return "message/editNotice";
+    }
+
+    @RequestMapping("noticeDetail")
+    public String noticeDetail(){
+        return "message/noticeDetail";
     }
 }
