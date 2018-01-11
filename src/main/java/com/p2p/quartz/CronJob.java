0108279a798bf41106c3a04771666191be4324be
ZhengYouzhen <1006747577@qq.com>
2018-01-11 08:41:18 +0800

    quartz配置

--- src/main/java/com/p2p/quartz/CronJob.java
+++ src/main/java/com/p2p/quartz/CronJob.java
@@ -11,6 +11,6 @@ public class CronJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("hello, cron!");
+        System.out.println("hello, cron!------------------------");
     }
 }
